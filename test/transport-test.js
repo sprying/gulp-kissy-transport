4ba2c3217ba580f0b87cbe7cbe63edc2d377304e
yingchun.fyc <yingchun.fyc@alibaba-inc.com>
2015-08-18 11:29:44 +0800

    增加单元测试

---
+++ test/transport-test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var transport = require('../index.js');
+var gulp = require('gulp');
+var fs = require('fs');
+var through = require('through2');
+describe('test begin',function(){
+	it('transport index.js should equal the target',function(done){
+		this.timeout(5000);
+		var targContent = fs.readFileSync('./test/build/app/index.js');
+		gulp.src(['./test/app/index.js'])
+		    .pipe(transport(['./test/app','./test/components']))
+		    .pipe(through.obj(function(file,encode,callback){
+				assert.equal(file.contents.toString(),targContent);	
+				done();
+				callback(null, file);
+		     }));
+	});
+});
